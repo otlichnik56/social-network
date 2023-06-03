4d0f8194934cf681ff483aa84d4668ce6848d758
Hyp <otlichnik56@yandex.ru>
2023-06-04 03:35:11 +0500

    c пагинацией, без описания

--- src/main/java/com/example/repository/PublicationRepository.java
+++ src/main/java/com/example/repository/PublicationRepository.java
@@ -1,13 +1,16 @@
 package com.example.repository;
 
 import com.example.entity.Publication;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
-public interface PublicationRepository extends JpaRepository<Publication, Long> {
+public interface PublicationRepository extends PagingAndSortingRepository<Publication, Long> {
     List<Publication> findByUsernameIgnoreCase(String username);
 
-    List<Publication> findByAuthorId(Long id);
+    @Query(value = "SELECT * FROM publications JOIN subscriptions ON publications.author_id = subscriptions.to_user JOIN users on subscriptions.from_user = users.id WHERE users.id = :id", nativeQuery = true)
+    List<Publication> findPublicationsMySubscriptions(Long id, Pageable pageable);
 
 }
--- src/main/java/com/example/service/PublicationService.java
+++ src/main/java/com/example/service/PublicationService.java
@@ -2,33 +2,31 @@ package com.example.service;
 
 import com.example.dto.Post;
 import com.example.entity.Publication;
-import com.example.entity.Subscription;
 import com.example.entity.User;
 import com.example.repository.PublicationRepository;
-import com.example.repository.SubscriptionRepository;
 import com.example.repository.UserRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 public class PublicationService {
 
     private final PublicationRepository publicationRepository;
     private final UserRepository userRepository;
-    private final SubscriptionRepository subscriptionRepository;
 
-    public PublicationService(PublicationRepository publicationRepository, UserRepository userRepository, SubscriptionRepository subscriptionRepository) {
+    public PublicationService(PublicationRepository publicationRepository, UserRepository userRepository) {
         this.publicationRepository = publicationRepository;
         this.userRepository = userRepository;
-        this.subscriptionRepository = subscriptionRepository;
     }
 
     public Publication getPublication(Long id) {
-        return publicationRepository.getReferenceById(id);
+        return publicationRepository.findById(id).orElse(null);
     }
 
     public List<Publication> getMyPublications(String username) {
@@ -63,7 +61,18 @@ public class PublicationService {
 
     // Возвращает публикации на кого подписан
     public List<Publication> getPublicationsMySubscriptions(String username, Integer offset, Integer limit) {
+        Pageable pageable = PageRequest.of(offset, limit, Sort.by("date").descending());
+        User user = userRepository.findByUsername(username).orElse(null);
+        if (user == null) {
+            return null;
+        } else {
+            return publicationRepository.findPublicationsMySubscriptions(user.getId(), pageable);
+        }
+    }
 
+
+    /**
+    public List<Publication> getPublicationsMySubscriptions(String username, Integer offset, Integer limit) {
         User user = userRepository.findByUsername(username).orElse(null);
         if (user == null) {
             return null;
@@ -82,7 +91,7 @@ public class PublicationService {
             Collections.reverse(publicationsSort);
             return publicationsSort;
         }
-    }
+    }  */
 
 
     // приватные методы класса
